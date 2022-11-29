20fcc1980c490fbb752e8424d2c3275e7410bc1a
Maggie <muriithinyawira13@gmail.com>
2022-11-29 19:46:32 +0300

    Completed Assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const form = document.getElementById('create-task-form');
+  form.addEventListener('submit', function(event){
+    event.preventDefault();
+    //const input = document.getElementById('new-task-description').value;
+    const taskList = document.getElementById('tasks');
+      const taskItem = document.createElement('li');
+      console.log(taskItem);
+        taskItem.innerHTML = event.target['new-task-description'].value;
+        taskList.append(taskItem);
+
+    const cancel = this.getElementsById
+  });
 });
